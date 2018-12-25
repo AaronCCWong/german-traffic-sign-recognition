978418e75fe2a908fe9cdfd8eca86e874a997689
AaronCCWong <aarn.wong@gmail.com>
2018-12-25 02:21:58 -0500

    Remove print statement

--- base_model.py
+++ base_model.py
@@ -16,7 +16,7 @@ class BaseNet(nn.Module):
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        print(x.shape)
+
         x = x.view(-1, 500)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
